aece6f74c23ba36b267f33e5f20a6b125868ebe2
Leeyoungsup <liive29252@gmail.com>
2025-07-03 14:26:58 +0900

    아이템 조회 엔드포인트 추가: item_id 및 쿼리 파라미터 지원

--- api.py
+++ api.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query, Path
 from fastapi import APIRouter
 from todo import todo_router
 from model import Item
 import os
 import uvicorn
+from typing import Optional, Annotated
 
 app = FastAPI()
 router = APIRouter()
@@ -26,6 +27,15 @@ app.include_router(todo_router)
 host = os.getenv('HOST', '127.0.0.1')
 port = int(os.getenv('PORT', '8080'))
 
+@app.get("/items/{item_id}")
+async def read_items(
+        item_id: int = Path(title="The ID of the item to get"),
+        q: Optional[str] = Query(default=None, alias="item-query")):
+    
+    results = {"item_id": item_id}
+    if q:
+        results.update({"q": q})
+    return results
 
 @app.post("/items/")
 async def create_item(item: Item):
